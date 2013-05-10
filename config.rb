b9ec49a3edfe02b3ca68a20ae4b976ed75cb52e9
Ralph Hansell <rhansell@pixelmedia.com>
2013-05-10 10:31:19 -0400

    sass folder renamed to scss. config.rb updated to match.

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ environment = :production
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "sass"
+sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
 
